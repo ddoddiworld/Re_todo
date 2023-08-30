cb67fb05299de0c2ab563368d5c2b91ee5934d1e
Emily Jang <93027502+ddoddiworld@users.noreply.github.com>
2023-08-30 21:22:00 +0900

    [추가] App.jsx에 react, redux import 요소들

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import Router from './shared/Router';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import {addTodo, deleteTodo, updateTodo} from "./redux/config/modules/todos"
 
 function App() {
   return (
